9a8ea7ca1f79933571db5c4ea7d22615104ea9f7
Deepanshu Khandelwal <122585913+dkc2006@users.noreply.github.com>
2024-11-10 13:17:31 +0530

    Week-1 Task [Prefix Sum Problems] completed

--- Week-1-Task/Prefix Sum Problems/2090. K Radius Subarray Averages.java
+++ Week-1-Task/Prefix Sum Problems/2090. K Radius Subarray Averages.java
@@ -1,23 +1,25 @@
+import java.util.Arrays;
+
 class Solution {
-  public int[] getAverages(int[] nums, int k) {
-    final int n = nums.length;
-    final int size = 2 * k + 1;
-    int[] ans = new int[n];
-    Arrays.fill(ans, -1);
-    if (size > n)
-      return ans;
+    public int[] getAverages(int[] nums, int k) {
+        final int n = nums.length;
+        final int size = 2 * k + 1;
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1);
+        if (size > n)
+            return ans;
 
-    long sum = 0;
+        long sum = 0;
 
-    for (int i = 0; i < size; ++i)
-      sum += nums[i];
+        for (int i = 0; i < size; ++i)
+            sum += nums[i];
 
-    for (int i = k; i + k < n; ++i) {
-      ans[i] = (int) (sum / size);
-      if (i + k + 1 < n)
-        sum += nums[i + k + 1] - nums[i - k];
+        for (int i = k; i + k < n; ++i) {
+            ans[i] = (int) (sum / size);
+            if (i + k + 1 < n)
+                sum += nums[i + k + 1] - nums[i - k];
+        }
+
+        return ans;
     }
-
-    return ans;
-  }
 }
